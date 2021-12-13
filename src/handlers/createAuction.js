203fd7f85d08a3d7863e725162daef4db4ad38eb
Christian  Turner <christian@huboo.co.uk>
2021-12-13 17:58:30 +0000

    added dynamo DB and updated createAuction to use dynamoDB, with errors

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,12 +1,23 @@
+import { v4 as uuid } from 'uuid';
+import aws from 'aws-sdk';
+
 async function createAuction(event, context) {
   const { title } = JSON.parse(event.body);
   const now = new Date();
 
   const auction = {
+    id: uuid(),
     title,
     status: 'OPEN',
     createdAt: now.toISOString()
   };
+
+  const dynamodb = new aws.DynamoDB.DocumentClient();
+
+  await dynamodb.put({
+    TableName: 'auctions_table',
+    Item: auction
+  }).promise();
 
   return {
     statusCode: 201,
